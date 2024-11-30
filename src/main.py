468b4fa826c65ac517ed9026734917e115ecb145
Jonathan <jonathan.bartolome@outlook.com>
2024-11-30 14:29:19 -0600

    Add special token handling. Text BPE tokenizer

--- src/main.py
+++ src/main.py
@@ -1,19 +1,10 @@
 import tokenizer
+import tiktoken
 
 with open("datasets/literature/the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-    vocab_model = tokenizer.Vocab(raw_text)
-    token_engine = tokenizer.Tokenizer(vocab_model.vocab)
-
-    # Test the vocab encoding.
-    test_text = """"It's the last he painted, you know," 
-       Mrs. Gisburn said with pardonable pride."""
-    ids = token_engine.encode(test_text)
-    print(ids)
-    print(token_engine.decode(ids))
-
-    # This test demonstrates need for large vocab sets.
-    # Will produce a KeyError
-    test_text_2 = "Hello, do you like tea?"
-    print(token_engine.encode(test_text_2))
+tiktokenizer = tiktoken.get_encoding("gpt2")
+enc_text = tiktokenizer.encode(raw_text)
+print(len(enc_text))
+enc_sample = enc_text[50:]
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -18,13 +18,51 @@ import re
 # Step 2 -- Converting Tokens into Token IDs
 # To map the previously generated tokens into token IDs, we have
 # to build a vocabulary first. This vocabulary defines how we map
-# each unique word and special character to a unique integer
+# each unique word and special character to a unique integer.
+
+# Take your training set, seperate by words/punctiations, sort by
+# alphabetic order. Assigned an integer value to each word and throw
+# this into a key/value structure - dictionary in python.
 
 # Step 3 -- Adding special context tokens
+# We add special tokens to a vocabulary to deal with certain context.
+# For instance, we add an <|unk|> token to represent new and
+# unknown words that were not part of the training data and thus not
+# part of the existing vocabulary. Furthermore, we add the
+# <|endoftext|> token that we can use to separate two unrelated
+# text sources.
+
+# There are other special tokens that researchers may use depending
+# on the model they are building, but the tokenizer used for GPT
+# models do not need any of these tokens, it only uses an <|endoftext|>
+# token for simplicity. The tokenizer used for GPT models also doesn't
+# use an <|unk|> token for out-of-vocabulary words. Instead, the model
+# uses byte pair encoding.
 
 
 _RE_ENCODE = r'([,.:;?_!"()\']|--|\s)'
 _RE_DECODE = r'\s+([,.?!"()\'])'
+
+_SPECIAL_TOKENS = [
+    "<|endoftext|>",
+    "<|unk|>",
+]
+
+
+class Text:
+    def __init__(self, text: str = ""):
+        self._text = text
+
+    @property
+    def text(self) -> str:
+        return self._text
+
+    @text.setter
+    def set_text(self, text: str) -> None:
+        self._text = text
+
+    def extend_text(self, text: str) -> None:
+        self._text = " <|endoftext|> ".join((self._text, text))
 
 
 class Vocab:
@@ -50,6 +88,7 @@ class Vocab:
         output = re.split(self._re_encode, self._text)
         tokens = [item.strip() for item in output if item.strip()]
         all_words = sorted(set(tokens))
+        all_words.extend(_SPECIAL_TOKENS)
         vocab = {token: integer for integer, token in enumerate(all_words)}
         return vocab
 
@@ -67,6 +106,12 @@ class Tokenizer:
 
     def encode(self, text) -> list[int]:
         preprocessed = self._tokenize(text)
+
+        # Unknown words are converted to <|unk|> special token
+        preprocessed = [
+            item if item in self._str_to_int else "<|unk|>" for item in preprocessed
+        ]
+
         return [self._str_to_int[s] for s in preprocessed]
 
     def decode(self, ids) -> str:
